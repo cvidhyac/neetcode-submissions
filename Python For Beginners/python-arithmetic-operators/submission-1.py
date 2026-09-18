ac3913efc25c703622dcbf3d9a7a3dd4d1b1757f
Vidhya Chidambaram <vidhya.chidambaram@gmail.com>
2026-09-18 15:44:04 -0400

    Add: python-arithmetic-operators - submission-1

---
+++ Python For Beginners/python-arithmetic-operators/submission-1.py
@@ -0,0 +1,9 @@
+a, b, c = 2, 2, 0.5
+sum_of_nums = a+b+c
+print(sum_of_nums)
+zero_minus_sum = 0 - sum_of_nums
+print(zero_minus_sum)
+product_nums = a * b * c
+print(product_nums)
+rem_nums = sum_of_nums / product_nums
+print(rem_nums)
