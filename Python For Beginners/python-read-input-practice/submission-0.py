17b525b79151f629ec4f88778d102df6bfc64ac5
Vidhya Chidambaram <vidhya.chidambaram@gmail.com>
2026-09-19 14:26:18 -0400

    Add: python-read-input-practice - submission-0

---
+++ Python For Beginners/python-read-input-practice/submission-0.py
@@ -0,0 +1,11 @@
+def add_two_numbers() -> int:
+    nums = input()
+    return sum([int(num) for num in nums.split(",")])
+
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
